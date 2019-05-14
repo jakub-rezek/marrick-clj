71a09f8e3e877287f5af3372ebdc749c57dccfb3
Jakub Rezek <rezek-ja@seznam.cz>
2019-05-14 13:14:03 -0700

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to marrick
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/marrick/core.clj
@@ -0,0 +1,31 @@
+(ns marrick.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+;;; pg 19/35/339
+
+(defn my-second [list]
+  (first (rest list)))
+
+(defn my-third [list]
+  (first (rest (rest list))))
+
+
+;;; pg 24/40/339
+
+(defn my-apply [fn seq]
+  (eval (cons fn seq)))
+
+;;; pg 26/42
+
+(defn my-sqr [n]
+  (* n n))
+
+(defn add-squares [& numbers]
+  (apply + (map my-sqr numbers)))
+
+(defn factorial [n]
+  (apply * (map inc (range n))))
